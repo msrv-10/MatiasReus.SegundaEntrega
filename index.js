e1d8301196bc182dd393fc045febab6419621217
msrv <matiassreus@gmail.com>
2022-10-24 22:09:11 -0300

    get all

--- index.js
+++ index.js
@@ -13,16 +13,15 @@ class Producto{
         this.precio = precio;
         this.thumbnail = thumbnail;
     }
-
+    
 }
 const productos = [];
 productos.push(new Producto('Martillo', 2500, './Martillo.png'));
 productos.push(new Producto('Espatula', 1500, './Espatula.png'));
 productos.push(new Producto('Destornillador', 1000, './Destornillador.png'));
-
 async function save(){
     try{
-        await productos.forEach((item, i) => {
+        productos.forEach((item, i) => {
             item.id = i + 1;
         });
         await fs.appendFile('./productos.txt', `${JSON.stringify(productos)}`, (err) =>{
@@ -38,9 +37,16 @@ async function save(){
     }
 }
 save();
-function getById(idToFind){
-     productos.find((p)=> p.id === idToFind);
-/*     try{
+setTimeout(()=>{
+    function getById(idToFind){
+    productos.find((p)=> p.id === idToFind);
+};
+console.log(getById(1));
+}, 2000)
+
+/*async function getById(idToFind){
+    try{
+        productos.find((p)=> p.id === idToFind);
         await
         if(err){
             console.log('No lo pude buscar');
@@ -48,5 +54,14 @@ function getById(idToFind){
             console.log('Lo encontre');
         }
     } */
-};
-console.log(getById(2));
+
+async  function getData() {
+    try {
+        const data = await fs.promises.readFile('./productos.txt',  'utf-8')
+        console.log(data);
+    }
+    catch(err){
+        console.log('error de lectura', err);
+    }
+}
+getData()
